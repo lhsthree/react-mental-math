400e2ba560d5823f5b937a4f25df25a3fb6a067a
root <lhsthree@gmail.com>
2021-04-22 01:01:06 +0000

    first commit

---
+++ src/Score.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Score = ({numCorrect, numQuestions}) => {
+  return (
+    <p className="text">
+      Your Score: {numCorrect}/{numQuestions}
+    </p>
+  );
+};
+
+export default Score;
